a4a5183431f2be73c80c37cb0bb7a810954b6a47
ofthegoats <47647338+ofthegoats@users.noreply.github.com>
2022-04-03 23:11:42 +0100

    feat: solve polynomial

--- src/Solve.hs
+++ src/Solve.hs
@@ -2,13 +2,36 @@ module Solve (solvePolynomial) where
 
 import Data.Complex
 import qualified Data.Map as M
+import GHC.List (iterate')
 import Types (Polynomial)
 
 {-
 Given a polynomial in the form M.Map Power Coefficient
-Find all the (real and complex) solutions approximately
+Find all (complex) solutions approximately
 Using During-Kerner: https://en.wikipedia.org/wiki/Durand%E2%80%93Kerner_method
 -}
 
 solvePolynomial :: Polynomial -> [Complex Double]
-solvePolynomial p = [1 :+ 1]
+solvePolynomial p =
+  let f = makeF p
+      order = fst . head $ M.toDescList p
+      base = [(0.4 :+ 0.9) ** (n :+ 0) | n <- [0 .. order - 1]]
+   in ( iterate'
+          ( \(s : sols) -> iteration s sols f 0 (1 + length sols)
+          )
+          base
+      )
+        !! 100
+
+makeF :: Polynomial -> (Complex Double -> Complex Double)
+makeF poly x = M.foldlWithKey' (\y p c -> y + ((c :+ 0) * (x ** (p :+ 0)))) 0 poly
+
+denom :: Complex Double -> [Complex Double] -> Complex Double
+denom x rest = product $ map (x -) rest
+
+iteration :: Complex Double -> [Complex Double] -> (Complex Double -> Complex Double) -> Int -> Int -> [Complex Double]
+iteration s sols@(next : rest) f current max
+  | current == max = s : sols
+  | otherwise =
+    let s' = s - f s / denom s sols
+     in iteration next (rest ++ [s']) f (succ current) max
